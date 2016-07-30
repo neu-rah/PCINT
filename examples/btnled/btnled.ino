fcb2a48c2a7bc9d52984d0539efa64ca5a78e2d4
Rui Azevedo <ruihfazevedo@gmail.com>
2016-07-30 09:18:29 +0000

    simple example

---
+++ examples/btnled/btnled.ino
@@ -0,0 +1,18 @@
+#include <pcint.h>
+
+#define led 13
+#define btn 12
+
+void setled() {
+  digitalWrite(led,digitalRead(btn));
+}
+
+void setup() {
+  pinMode(led,OUTPUT);
+  pinMode(btn,INPUT);
+  digitalWrite(btn,1);//pullup
+  PCattachInterrupt(btn,setled,CHANGE);
+  setled();//initial led status
+}
+
+void loop() {}
